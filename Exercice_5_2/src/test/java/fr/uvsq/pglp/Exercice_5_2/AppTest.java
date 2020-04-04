d97639cac08388c834eaef58219c06d8af6adda8
RachData <rachidderra@gmail.com>
2020-04-04 21:43:35 +0200

    creation du projet

---
+++ Exercice_5_2/src/test/java/fr/uvsq/pglp/Exercice_5_2/AppTest.java
@@ -0,0 +1,20 @@
+package fr.uvsq.pglp.Exercice_5_2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
